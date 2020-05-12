0eb7629934d455860a038b31c1a5a2d9f61c37f4
Itamar Dori <itamardori@local>
2020-05-12 10:55:43 +0300

    removed unnessecary mutex

--- winston-splunk-hec/index.js
+++ winston-splunk-hec/index.js
@@ -1,59 +1,6 @@
 const winston = require('winston');
 const Transport = require('winston-transport');
 const axios = require('axios');
-const { Semaphore } = require('async-mutex');
-
-const MAX_CONCURRENT_LOG_WRITES = 100;
-
-class LogBuffer {
-    constructor() {
-        this.buff = '';
-        this.writeMutex = new Semaphore(MAX_CONCURRENT_LOG_WRITES);
-    }
-    /**
-     * Pushes a log to the buffer, scheduled to be sent later.
-     * @param {*} log 
-     */
-    async push(log) {
-        const [value, release] = await this.writeMutex.acquire();
-        try {
-            if(typeof log == "object") {
-              log = JSON.stringify(log);
-            }
-
-            this.buff = this.buff.concat(JSON.stringify(log))
-
-          } finally {
-            release();
-        }
-    }
-
-    length() {
-      return this.buff.length;
-    }
-
-    /**
-     * Fluses the buffer, returning the buffer's contents
-     */
-    async flush() {
-        const releases = [];
-        let flushedData;
-
-        try {
-            for (let i = 0; i < MAX_CONCURRENT_LOG_WRITES; i++) {
-                const [value, release] = await this.writeMutex.acquire();
-                releases.push(release);
-            } 
-
-            flushedData = this.buff;
-            this.buff = '';
-        } finally {
-            releases.forEach(release => release());
-        }
-
-        return flushedData;
-    }
-}
 
 class SplunkHec extends Transport {
   constructor(opts) {
@@ -63,7 +10,7 @@ class SplunkHec extends Transport {
     this.bufferLimit = opt.bufferLimit || null;
     this.splunkUrl = opt.splunkUrl;
     this.splunkHec = opt.splunkHec;
-    this.buff = new LogBuffer(this.bufferLimit);
+    this.buff = '';
     this.requestConfig = {
       method: 'post',
       url: opt.splunkUrl,
@@ -71,15 +18,18 @@ class SplunkHec extends Transport {
       timeout: opt.flushInterval, 
   }
     if(this.buffering) {
-      registerIntervals();
+      setInterval(this.sendLogs, this.flushInterval);
     }
   }
 
   async sendLogs(logs) {
-    let logsToSend = this.buffering ? await this.buff.flush() : logs;
+    let logsToSend;
 
-    if(typeof logsToSend == "object") {
-        logsToSend = JSON.stringify(logsToSend);0
+    if(this.buffering) {
+      logsToSend = this.buff;
+      this.buff = '';
+    } else {
+      logsToSend = typeof logs == "object" ? JSON.stringify(logs) : logs;
     }
 
     try {
@@ -87,27 +37,24 @@ class SplunkHec extends Transport {
             await axios({...this.requestConfig, data: logsToSend})
         }
     } catch (err) {
-        await buff.push(logsToSend);
+        this.buff.concat(logsToSend);
         console.error(`[winston:splunk] Could not send logs to splunk, err: ${err}`)
     }
   }; 
-   registerIntervals() {
-      setInterval(this.sendLogs, this.flushInterval);
-  }
 
   log(info, callback) {
     setImmediate(() => {
       this.emit('logged', info);
     });
-
+    const toLog = typeof info == "object" ? JSON.stringify(info) : info;
     if(this.buffering) {
-      this.buff.push(info);
+      this.buff.concat(toLog);
 
-      if(this.bufferLimit && this.buff.length() > this.bufferLimit) {
+      if(this.bufferLimit && this.buff.length > this.bufferLimit) {
         this.sendLogs();
       }
     } else {
-      this.sendLogs(info);
+      this.sendLogs(toLog);
     }
 
     callback();
